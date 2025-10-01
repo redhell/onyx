ccf91edd4e86e94295823788fc17ba88588a8220
Richard Guan <rguan72@gmail.com>
2025-10-01 15:02:04 -0700

    .

--- backend/onyx/chat/turn/infra/packet_translation.py
+++ backend/onyx/chat/turn/infra/packet_translation.py
@@ -3,7 +3,6 @@ from agents import RawResponsesStreamEvent
 from onyx.server.query_and_chat.streaming_models import MessageDelta
 from onyx.server.query_and_chat.streaming_models import MessageStart
 from onyx.server.query_and_chat.streaming_models import PacketObj
-from onyx.server.query_and_chat.streaming_models import SectionEnd
 
 
 def default_packet_translation(ev: object) -> PacketObj | None:
@@ -11,11 +10,9 @@ def default_packet_translation(ev: object) -> PacketObj | None:
         # TODO: might need some variation here for different types of models
         # OpenAI packet translator
         obj: PacketObj | None = None
-        if ev.data.type == "response.created":
+        if ev.data.type == "response.content_part.added":
             obj = MessageStart(type="message_start", content="", final_documents=None)
         elif ev.data.type == "response.output_text.delta":
             obj = MessageDelta(type="message_delta", content=ev.data.delta)
-        elif ev.data.type == "response.output_item.done":
-            obj = SectionEnd(type="section_end")
         return obj
     return None
